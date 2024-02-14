7b454d5277e5f3c563f6504cb8a638040c4f5e8f
ecs90 <ecs_salas@hotmail.com>
2024-02-14 11:41:00 +0100

    HelloWorld methods created

--- S04T01N02SalasEzequiel/src/main/java/cat/itacademy/barcelonactiva/salas/ezequiel/s04/t01/n02/Controllers/HelloWorldController.java
+++ S04T01N02SalasEzequiel/src/main/java/cat/itacademy/barcelonactiva/salas/ezequiel/s04/t01/n02/Controllers/HelloWorldController.java
@@ -1,4 +1,22 @@
 package cat.itacademy.barcelonactiva.salas.ezequiel.s04.t01.n02.Controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorldController {
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(defaultValue = "UNKNOWN") String nom){
+        return "Hola, " + nom + ". Estàs executant un projecte Gradle";
+    }
+
+    @GetMapping(value = {"/HelloWorld2/{nom}", "HelloWorld2"})
+    public String saluda2(@PathVariable(required = false) String nom){
+        if (nom == null) {
+            nom = "UNKNOWN";
+        }
+        return "Hola, " + nom + ". Estàs executant un projecte Gradle";
+    }
 }

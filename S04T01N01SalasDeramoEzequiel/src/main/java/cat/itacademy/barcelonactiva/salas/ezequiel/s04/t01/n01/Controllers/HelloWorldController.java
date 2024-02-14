2bf698481503517020ecb504cbfd02c3e046e33d
ecs90 <ecs_salas@hotmail.com>
2024-02-14 11:26:55 +0100

    Initial commit

---
+++ S04T01N01SalasDeramoEzequiel/src/main/java/cat/itacademy/barcelonactiva/salas/ezequiel/s04/t01/n01/Controllers/HelloWorldController.java
@@ -0,0 +1,22 @@
+package cat.itacademy.barcelonactiva.salas.ezequiel.s04.t01.n01.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(defaultValue = "UNKNOWN") String nom){
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
+
+    @GetMapping(value = {"/HelloWorld2/{nom}", "HelloWorld2"})
+    public String saluda2(@PathVariable(required = false) String nom){
+        if (nom == null) {
+            nom = "UNKNOWN";
+        }
+        return "Hola, " + nom + ". Estàs executant un projecte Maven";
+    }
+}
---
+++ S04T01N02SalasEzequiel/src/main/java/cat/itacademy/barcelonactiva/salas/ezequiel/s04/t01/n02/Controllers/HelloWorldController.java
@@ -0,0 +1,4 @@
+package cat.itacademy.barcelonactiva.salas.ezequiel.s04.t01.n02.Controllers;
+
+public class HelloWorldController {
+}
